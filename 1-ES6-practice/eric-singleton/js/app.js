4104ff32524c26ee1735fa081c27ea327f659de5
Eric Ivan Singleton Junior <eric.singleton.jr@gmail.com>
2018-03-13 19:40:09 -0700

    Added template literals

--- 1-ES6-practice/eric-singleton/js/app.js
+++ 1-ES6-practice/eric-singleton/js/app.js
@@ -16,7 +16,7 @@ const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/${name}.jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -36,7 +36,9 @@ function displayPics(){
   }
   // TODO: In a sentence or two, explain why the previous line of code threw an error when we changed the variable declaration from `const to `const`.
   // PUT YOUR RESPONSE IN THIS COMMENT
+  //######
   //Since rando is now declared with const, the scope is only defined within the while loop scope so it cannot be accessed outside
+  //######
   console.log(viewed);
 
   // To the DOM and beyond!
@@ -62,7 +64,7 @@ function handleClick(event) {
   for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(`${event.target.id} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -73,7 +75,7 @@ function handleClick(event) {
 function showList() {
   for(let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = `${allProducts[i].name} has ${allProducts[i].votes} votes in ${allProducts[i].views} views`;
     list.appendChild(liEl);
   }
 }

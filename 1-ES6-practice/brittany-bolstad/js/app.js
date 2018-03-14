2bf9c7dd54bbb98f78105c37e94c59b66eaa678c
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-03-13 19:28:03 -0700

    changed more to let and converted to literal notation

--- 1-ES6-practice/brittany-bolstad/js/app.js
+++ 1-ES6-practice/brittany-bolstad/js/app.js
@@ -10,13 +10,13 @@ const pics = [document.getElementById('left'),
                 document.getElementById('center'), //eslint-disable-line
                 document.getElementById('right')]; //eslint-disable-line
 const list = document.getElementById('productlist');
-const totalClicks = 0;
+let totalClicks = 0;
 const views = [];
 const votes = [];
 
 function Product(name) {
   this.name = name;
-  this.path = 'img/' + name + '.jpg';
+  this.path = `img/` + name + `.jpg`;
   this.votes = 0;
   this.views = 0;
   allProducts.push(this);
@@ -61,10 +61,10 @@ function handleClick(event) {
     return alert('Be sure to click directly on an image!!');
   }
   totalClicks += 1;
-  for(const i = 0; i < names.length; i++){
+  for(let i = 0; i < names.length; i++){
     if(event.target.id === allProducts[i].name) {
       allProducts[i].votes += 1;
-      console.log(event.target.id + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views');
+      console.log(event.target.id + ` has ` + allProducts[i].votes + ` votes in ` + allProducts[i].views + ` views`);
     }
   }
   localStorage.busmall = JSON.stringify(allProducts);
@@ -73,9 +73,9 @@ function handleClick(event) {
 }
 
 function showList() {
-  for(const i = 0; i < allProducts.length; i++) {
+  for(let i = 0; i < allProducts.length; i++) {
     const liEl = document.createElement('li');
-    liEl.textContent = allProducts[i].name + ' has ' + allProducts[i].votes + ' votes in ' + allProducts[i].views + ' views';
+    liEl.textContent = allProducts[i].name + ` has ` + allProducts[i].votes + ` votes in ` + allProducts[i].views + ` views`;
     list.appendChild(liEl);
   }
 }
@@ -130,7 +130,7 @@ if(localStorage.busmall){
   allProducts = JSON.parse(localStorage.busmall);
 } else {
   console.log('There is no local storage data; initialize app by creating instances');
-  for(const i = 0; i < names.length; i++) {
+  for(let i = 0; i < names.length; i++) {
     new Product(names[i]);
   }
   console.log(allProducts);
